bfe1b0e31a0ac73a3c45a575ff7bfd0244c4b30a
sonualam <sonualam1599@gmail.com>
2024-08-05 08:46:16 +0530

    add starter template ready

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import PostPage from "./components/PostPage";
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white pt-3 ">
+      <PostPage />
+    </div>
+  );
+}
+
+export default App;
